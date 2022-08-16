1f22d099d6b6bc85946dc7dbcdd85028969584de
Richard Ye <richardyeee@outlook.com>
2022-08-16 14:50:59 -0400

    added files

---
+++ encoder.js
@@ -0,0 +1,13 @@
+window.onload = startFunction;
+
+function startFunction() {
+  var thisButton = document.getElementById("doIt");
+
+  thisButton.addEventListener("click", function () {
+    const string = document.getElementById("inputText").value;
+    // Encode the string using the he package's encode() function
+    var encodedStr = he.encode(string);
+
+    document.getElementById("output").textContent = encodedStr;
+  });
+}
